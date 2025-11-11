ce73d75682c4f388a2169caa1ee124bc825610d7
wielauritz <socialmedia@wwwiebusch.de>
2025-11-11 16:33:35 +0100

    Add storage instance retrieval from window for API helper

--- frontend/js/api-helper.js
+++ frontend/js/api-helper.js
@@ -10,6 +10,9 @@ const API_BASE_URL_V2 = 'https://api.new.nora-nak.de/v2';
 // const API_BASE_URL = 'http://localhost:8000/v1';
 // const API_BASE_URL_V2 = 'http://localhost:8000/v2';
 
+// Get storage instance from window (exported by storage-manager.js)
+const storage = window.storage;
+
 /**
  * Helper: Detect if running in Capacitor
  */
